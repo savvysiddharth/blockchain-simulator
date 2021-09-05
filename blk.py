506b7b6b372f0fe40d8b6da8fd1a90c12688bb16
yasaswini dasari <tcs15b010@iittp.ac.in>
2021-09-05 11:10:28 +0530

    Add files via upload

---
+++ blk.py
@@ -0,0 +1,33 @@
+import time
+import hashlib
+
+
+class Block (object):
+    def __init__(self, transactions):
+        self.time = time.time();  # Time block was created
+        self.index = hashlib.sha256(str(self.time)).hexdigest(); #Block Number
+        self.transactions = transactions; #Transaction Data
+        self.prevblk_link=None
+
+class Blockchain (object):
+    def __init__(self):
+        self.chain = []; #Blockchain is an array of Blocks
+
+    def getLastBlock(self):
+        return  self.chain[-1]
+
+    def addBlock(self, block):
+        if (len(self.chain) > 0):
+            block.prevblk_link = self.getLastBlock().index;
+        else:
+            block.prevblk_link = None
+        self.chain.append(block);
+
+
+class Transactions(object):
+    def __init__(self, sender, reciever, amt):
+        self.sender = sender;
+        self.reciever = reciever;
+        self.amt = amt;
+        self.time = time.time();
+        self.trxnid = hashlib.sha256(str(self.time)).hexdigest();
